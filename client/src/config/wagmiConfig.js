afc0e8bc017d4d93d6d53585c21b2414c386270f
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-03-06 20:35:52 +0500

    hardhat network added

--- client/src/config/wagmiConfig.js
+++ client/src/config/wagmiConfig.js
@@ -2,11 +2,11 @@ import { createClient, configureChains } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultWallets } from '@rainbow-me/rainbowkit';
-import { mainnet, goerli } from 'wagmi/chains';
+import { mainnet, goerli, hardhat } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 
 export const { chains, provider, webSocketProvider } = configureChains(
-    [goerli, mainnet],
+    [goerli, hardhat, mainnet],
     [
         alchemyProvider({ apiKey: process.env.REACT_APP_ETHERSCAN_API_KEY }),
         publicProvider()
