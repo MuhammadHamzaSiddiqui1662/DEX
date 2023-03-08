c79af0e72afc733d8a8f13ac720c2742cbb15631
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-03-09 02:35:07 +0500

    charts labels updated

--- client/src/components/Market/Market.js
+++ client/src/components/Market/Market.js
@@ -11,30 +11,32 @@ export const Market = ({ tokens, selectedToken }) => {
     const { trades, batTrades, repTrades, zrxTrades, isLoading } = useMarket(tokens);
     const mainData = selectedToken.symbol === "BAT" ? batTrades : selectedToken.symbol === "REP" ? repTrades : zrxTrades;
     const otherData = selectedToken.symbol === "BAT" ? [repTrades, zrxTrades] : selectedToken.symbol === "REP" ? [batTrades, zrxTrades] : [batTrades, repTrades];
+    const otherDataLabels = selectedToken.symbol === "BAT" ? ["REP", "ZRX"] : selectedToken.symbol === "REP" ? ["BAT", "ZRX"] : ["BAT", "REP"];
     return !isLoading ? (
         <div className="market">
-            <div className='mainChart'><ResponsiveContainer width={"100%"} height={380}>
-                <LineChart data={mainData}>
-                    <Line type="monotone" dataKey="price" stroke="#00ff22" />
-                    <XAxis dataKey="date" type='category'>
-                        <Label value="Pages of my website" position="insideBottom" offset={0} />
-                    </XAxis>
-                    <YAxis dataKey="price">
-                        <Label value="Pages of my website" domain={[0, 'dataMax']} angle={-90} offset={10} position="insideBottomLeft" />
-                    </YAxis>
-                    <Tooltip contentStyle={contentStyle} />
-                </LineChart>
-            </ResponsiveContainer>
+            <div className='mainChart'>
+                <ResponsiveContainer width={"100%"} height={380}>
+                    <LineChart data={mainData}>
+                        <Line type="monotone" dataKey="price" stroke="#00ff22" />
+                        <XAxis dataKey="date" type='category'>
+                            <Label value="Timestamp" position="insideBottom" offset={0} />
+                        </XAxis>
+                        <YAxis dataKey="price">
+                            <Label value={`${selectedToken.symbol} Prices`} domain={[0, 'dataMax']} angle={-90} offset={10} position="insideBottomLeft" />
+                        </YAxis>
+                        <Tooltip contentStyle={contentStyle} />
+                    </LineChart>
+                </ResponsiveContainer>
             </div>
             <div className='otherCharts'>
                 <ResponsiveContainer width={"45%"} height={300}>
                     <LineChart data={otherData[0]}>
                         <Line type="monotone" dataKey="price" stroke="#00ff22" />
-                        <XAxis dataKey="timeStamp">
-                            <Label value="Pages of my website" offset={0} position="insideBottom" />
+                        <XAxis dataKey="date">
+                            <Label value="Timestamp" offset={0} position="insideBottom" />
                         </XAxis>
                         <YAxis>
-                            <Label value="Pages of my website" angle={-90} offset={10} position="insideBottomLeft" />
+                            <Label value={`${otherDataLabels[0]} Prices`} angle={-90} offset={10} position="insideBottomLeft" />
                         </YAxis>
                         <Tooltip contentStyle={contentStyle} />
                     </LineChart>
@@ -42,11 +44,11 @@ export const Market = ({ tokens, selectedToken }) => {
                 <ResponsiveContainer width={"45%"} height={300}>
                     <LineChart data={otherData[1]}>
                         <Line type="monotone" dataKey="price" stroke="#00ff22" />
-                        <XAxis dataKey="timeStamp">
-                            <Label value="Pages of my website" offset={0} position="insideBottom" />
+                        <XAxis dataKey="date">
+                            <Label value="Timestamp" offset={0} position="insideBottom" />
                         </XAxis>
                         <YAxis>
-                            <Label value="Pages of my website" angle={-90} offset={10} position="insideBottomLeft" />
+                            <Label value={`${otherDataLabels[1]} Prices`} angle={-90} offset={10} position="insideBottomLeft" />
                         </YAxis>
                         <Tooltip contentStyle={contentStyle} />
                     </LineChart>
